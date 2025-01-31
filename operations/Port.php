55e359178a776209593ad38979021bff90138afa
SuperGoodMike <mike_mitchell@outlook.com>
2025-01-31 17:58:55 -0600

    Update Port.php

--- operations/Port.php
+++ operations/Port.php
@@ -2,41 +2,47 @@
 
 include_once('./OperationInterface.php');
 
+class Port implements OperationInterface {
+    private $ports = [25, 53, 80, 443, 465, 587, 993]; // Default ports
 
-class Port implements OperationInterface{
-  private $port;
-  private $ports = [25, 53, 80 , 443, 465, 587, 993]; // Default ports
-  
-  function __construct($port = 80) {
-    if ($port == ""){
-      $this->port = $this->ports;
-    } else {
-      $this->port = [$port];
+    function __construct($ports = "80") {
+        if ($ports == "") {
+            $this->ports = $this->ports;
+        } else {
+            $this->ports = $this->parsePorts($ports);
+        }
     }
-  }
-  
-  function getOutput($hostname) {
-    $port =  $this->port;
-    $portArray = "";
-    for ($i = 0; $i < count($port); $i++) {
-      $fp = fsockopen($hostname, $port[$i], $errno, $errstr, 5);
 
-      if ($fp) {
-        $result = 'open';
-        fclose($fp);
-      } else {
-        $result = 'closed';;
-      }
-      $portArray .= "\"$port[$i]\": \"Is $result\",\n";
+    private function parsePorts($ports) {
+        $portArray = [];
+        $ranges = explode(',', $ports);
+        foreach ($ranges as $range) {
+            if (strpos($range, '-') !== false) {
+                list($start, $end) = explode('-', $range);
+                $portArray = array_merge($portArray, range($start, $end));
+            } else {
+                $portArray[] = (int) $range;
+            }
+        }
+        return $portArray;
     }
-    $result = "[{\n";
-    $result .= rtrim($portArray, ",\n");
-    $result .= "\n}]";
-    return $result;
-  }
-    
+
+    function getOutput($hostname) {
+        $portArray = "";
+        foreach ($this->ports as $port) {
+            $fp = @fsockopen($hostname, $port, $errno, $errstr, 5);
+            if ($fp) {
+                $result = 'open';
+                fclose($fp);
+            } else {
+                $result = 'closed';
+            }
+            $portArray .= "\"$port\": \"Is $result\",\n";
+        }
+        $result = "[{\n";
+        $result .= rtrim($portArray, ",\n");
+        $result .= "\n}]";
+        return $result;
+    }
 }
-  
-  
-  ?>
-  
+?>
